fa7f8d5e87bdde3920d8e9d0fb5aeb031e0351cc
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2022-12-29 15:32:30 +0100

    Add programming concepts part

--- programming_concepts/README.md
+++ programming_concepts/README.md
@@ -9,3 +9,33 @@
 - Shadowing: we can declare a variable and use another variable with the same name. Shadowed variables can be different in differnt scopes. Using shadowing is permitted only if we use the `let` keyword. This is an important difference with the `mut` usage.
 
 #### Data types
+
+---
+
+#### Functions
+
+- `fn` keyword to define a function:
+
+  ```
+  fn main() {
+      println!("Hello, world!");
+
+      another_function();
+  }
+
+  fn another_function() {
+      println!("Another function.");
+  }
+  ```
+
+- Function definition ordering does not matter to rust compiler.
+- Function parameters must have a type.
+- Function bodies are made up of a series of statements optionally ending in an expression
+- Statements are instructions that perform some action and do not return a value. Expressions evaluate to a resulting value.
+- A new scope block created with curly brackets is an expression
+- We can write functions with return values simply evaluating the last expression of the function or using the `return` keyword.
+- The main difference between statemenet and expression is that the statement does not return anything, while expression does.
+
+#### Control flow
+
+- Rust does not perform automatic conversion of non boolean variables when evaluating conditions. >We must eplicit boolean expression along with if clauses.
--- programming_concepts/src/main.rs
+++ programming_concepts/src/main.rs
@@ -1,5 +1,23 @@
 const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
+fn another_function(x: i32, character: char) {
+    println!("Another function called. The values of x is {x}, that is associated with the {character} char.");
+}
+
+fn sum(x: f64, y: f64) -> f64 {
+    return x + y;
+}
+
+fn collection_looping(list: &[i32]) {
+    let mut index = 0;
+
+    while index < list.len() {
+        println!("the value is: {}", list[index]);
+
+        index += 1;
+    }
+}
+
 fn main() {
     // Mutability
     let mut x = 5;
@@ -28,4 +46,50 @@ fn main() {
 
     // Data types
     let _guess: u32 = "42".parse().expect("Not a number!");
+
+    let mut t: u128 = 100;
+    t += 200;
+    println!("{t}");
+
+    // Functions
+    another_function(1, 'a');
+    another_function(2, 'b');
+    another_function(3, 'c');
+
+    // Difference between statement and epression
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+    println!("The result for sum is: {}", sum(10.0, 476.43));
+
+    // Control flow
+    let number = 1;
+    let check_number = 1000;
+    if number > check_number {
+        println!("{} is greater than 100", number)
+    } else {
+        println!("{} is less than 100", number)
+    }
+
+    // Ternary operator
+    let condition = true;
+    let number = if condition {
+        let x = 10;
+        x * 10 - 7
+    } else {
+        let x = 10;
+        x * 10 + 7
+    };
+
+    println!("The value of number is: {number}");
+
+    // Looping
+    let a = [1, 2, 3, 4, 5, 6];
+    let b = [3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5];
+
+    collection_looping(&a);
+    collection_looping(&b)
 }
